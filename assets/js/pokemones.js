c9123793a6d1f2d08c5a8ad90deb300abdc00470
Mauricio Badilla <mbadilla86@gmail.com>
2021-04-13 11:58:35 -0600

    add pokemons link

--- assets/js/pokemones.js
+++ assets/js/pokemones.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
+  const state = {
+    pokemons: []
+  }
+  const pokemons = document.querySelector('ul.pokemons')
+
   function fetchPokemons(){
     fetch('https://pokeapi.co/api/v2/pokemon/', {
       method: 'GET',
@@ -6,6 +11,34 @@ document.addEventListener('DOMContentLoaded', function(){
         'Content-Type': 'application/json'
       }
     }).then(function(data){return data.json() })
-      .then(function(){})
+      .then(function(pokemons){ 
+        state.pokemons = pokemons.results
+        renderPokemons()
+      })
   }
+
+  function renderPokemons(){
+    state.pokemons.map(function(poke){
+      const li = document.createElement('li')
+      li.className = 'list-group-item'
+
+      const link = document.createElement('a')
+      link.className = 'pokemon-detail d-flex justify-content-between align-items-center'
+      link.href = poke.url
+
+      const text = document.createTextNode(poke.name)
+
+      const span = document.createElement('span')
+      span.className = 'badge badge-primary badge-pill'
+      span.innerText = '›'
+
+      link.appendChild(text)
+      link.appendChild(span)
+      li.appendChild(link)
+
+      pokemons.appendChild(li)
+    })
+  }
+
+  fetchPokemons()
 })
